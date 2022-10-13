38ce6f6f8560b5d3396b8d46b2043ddd21c0598f
unknown <sergiodiaz@latechfactory.com>
2022-10-13 02:38:53 -0300

    Dream Sound - Sergio Diaz

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -24,7 +24,7 @@ const Search = () => {
   return (
     <div ref={divRef} className="flex flex-col">
       <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
-        Showing results for <span className="font-black">{searchTerm}</span>
+        Showing results for <span className="font-black text-cyan-400">{searchTerm}</span>
       </h2>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {songs?.map((song, i) => (
